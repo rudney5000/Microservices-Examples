1a01836cd55bfbce61c4bd8f09f6beb48a7c9b35
Your Name <you@example.com>
2023-03-26 05:18:48 +0300

    cassandra-config

---
+++ microservice-gestiondeticket/account-service/src/main/java/com/dedytech/accountservice/config/CassandraConfiguration.java
@@ -0,0 +1,2 @@
+package com.dedytech.accountservice.config;public class CassandraConfiguration {
+}
---
+++ microservice-gestiondeticket/account-service/src/main/java/com/dedytech/accountservice/repository/AccountRepository.java
@@ -0,0 +1,2 @@
+package com.dedytech.accountservice.repository;public interface AccountRepository {
+}
